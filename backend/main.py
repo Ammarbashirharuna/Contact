496431c1f4bcb48d62dc04b9482b59d44ce448e7
ammar <ammarbashirharuna2020@gmail.com>
2024-12-17 16:21:04 +0100

    creating route for updating contact

--- backend/main.py
+++ backend/main.py
@@ -1,13 +1,13 @@
 from flask import Flask, request, jsonify
 from config import app, db
 from models import Contact
-
+# creating route for getting all contacts
 @app.route("/contacts", methods=["GET"])
 def get_contacts():
     contacts = Contact.query.all()
     json_contacts = list(map(lambda x: x.to_json(), contacts))
     return jsonify({"contacts": json_contacts})
-
+# creating route for creating a contact
 @app.route("create_contact", methods=["POST"])
 def create_contact():
     first_name = request.json.get("first_name")
@@ -26,6 +26,7 @@ def create_contact():
         return jsonify({"error": str(e)}), 500
     
     return jsonify({"message": "Contact created successfully"}), 201
+# creating route for updating a contact
 @app.route("/update_contact/<int:uset_id>")
 def update_contact(user_id):
     contact = Contact.query.get(user_id)
@@ -33,6 +34,14 @@ def update_contact(user_id):
         return jsonify({"error": "Contact not found"}), 404
 data = request.json
 contact.first_name = data.get("first_name", contact.first_name)
+contact.last_name = data.get("last_name", contact.last_name)
+contact.email = data.get("email", contact.email)
+
+db.session.commit()
+
+return jsonify({"message": "Contact updated successfully"}), 200
+# creating route for deleting a contact
+@app.route("/delete_contact/<int:user_id>", methods=["DELETE"])
 
 
 
