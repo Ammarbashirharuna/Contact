6ae52d2b31b197a5d424950bf073b17958e484e3
ammar <ammarbashirharuna2020@gmail.com>
2024-12-17 15:55:37 +0100

    creating adding contact route

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,4 @@
-form flask import Flask, request, jsonify
+from flask import Flask, request, jsonify
 from config import app, db
 from models import Contact
 
@@ -7,6 +7,32 @@ def get_contacts():
     contacts = Contact.query.all()
     json_contacts = list(map(lambda x: x.to_json(), contacts))
     return jsonify({"contacts": json_contacts})
+
+@app.route("create_contact", methods=["POST"])
+def create_contact():
+    first_name = request.json.get("first_name")
+    last_name = request.json.get("last_name")
+    email = request.json.get("email")
+    
+    if nor first_name or not last_name or not email:
+        return (
+            jsonify({"error": "Missing required fields"}), 400,
+        )
+    new_contact = Contact(first_name=first_name, last_name=last_name, email=email)
+    try:
+        db.seeion.add(new_contact)
+        db.session.commit()
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+    
+    return jsonify({"message": "Contact created successfully"}), 201
+@app.route("/update_contact/<int:uset_id>")
+def update_contact(user_id):
+    contact = Contact.query.get(user_id)
+    if not contact:
+        return jsonify({"error": "Contact not found"}), 404
+data = request.json
+contact.first_name = data.get("first_name", contact.first_name)
 
 
 
--- backend/models.py
+++ backend/models.py
@@ -1,11 +1,10 @@
 from config import db
 
-class Contact(db.model):
-    id = db.column(db.Integer, primary_key=True)
-    first_name = db.coloumn(db.stringg(80), unique=False, nullable=False)
-    last_name = db.coloumn(db.stringg(80), unique=False, nullable=False)
-    emai = db.coloumn(db.stringg(120), unique=True, nullable=False)
-    
+class Contact(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(80), unique=False, nullable=False)
+    last_name = db.Column(db.String(80), unique=False, nullable=False)
+    emai = db.Column(db.String(120), unique=True, nullable=False)
     def to_json(self):
         return {
             "id": self.id,
